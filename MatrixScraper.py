817d2f61b86b5de361c5b19f952a3699b2441faa
muke101 <muke101@protonmail.com>
2019-06-21 20:35:19 +0100

    figured out I was doing it right all this time

--- MatrixScraper.py
+++ MatrixScraper.py
@@ -1,15 +1,17 @@
-from bs4 import BeautifilSoup
+from bs4 import BeautifulSoup
+import re
 
 page = open('T1028','rb')
-soup = BeautifulSoup(page, 'html.parser')
 
-className = ['card', 'col-md4 description-body']
+classes = ['card', 'col-md4 description-body']
+
 
 def pageScraper(url):
+    #insert requesting here
     soup = BeautifulSoup(url, 'html.parser')       
     for className in classes:
-        for tag in soup.find(class_=className):
+        for tag in soup.find_all(class_='card-data'):
+                print([i.get_text() for i in tag.children])
+#    for tag in soup.find_all(class_='pt-3'): #this class in unique so iterating seperately in a less 'modular' way
             
-    for tag in soup.find_all(class_='pt-3'): #this class in unique so iterating seperately in a less 'modular' way
-            
-
+pageScraper(page)
